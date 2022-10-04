d96341f08f718f900dacac3c4af5fb731570ec71
Jan Kollert <jan.kollert@digital.cz>
2022-10-04 13:44:05 +0200

    Enabled custom category selection elements

--- lib/ui/pages/category_selection/category_selection_cubit.dart
+++ lib/ui/pages/category_selection/category_selection_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:voice_pad/data/models/voices_category.dart';
+import 'package:voice_pad/data/repositories/custom_voices_repository.dart';
 import 'package:voice_pad/data/repositories/preset_voices_repository.dart';
 import 'package:voice_pad/utils/injector.dart';
 import 'package:voice_pad/utils/page_state.dart';
@@ -11,14 +12,14 @@ class CategorySelectionCubit extends Cubit<PageState<List<VoicesCategory>>> {
     emit(state.copyWith(isLoading: true));
 
     final PresetVoicesRepository presetRepo = injector();
-    // final CustomVoicesRepository customRepo = injector();
+    final CustomVoicesRepository customRepo = injector();
 
     final categories = List<VoicesCategory>.empty(growable: true);
 
     final presetCategories = await presetRepo.getCategories();
     categories.addAll(presetCategories);
-    // final customCategories = await customRepo.getCategories();
-    // categories.addAll(customCategories);
+    final customCategories = await customRepo.getCategories();
+    categories.addAll(customCategories);
 
     emit(state.copyWith(isLoading: false, data: categories));
   }
--- lib/ui/pages/category_selection/category_selection_page.dart
+++ lib/ui/pages/category_selection/category_selection_page.dart
@@ -36,49 +36,76 @@ class CategorySelectionPage extends StatelessWidget {
               controller: _scrollController,
               shrinkWrap: true,
               children: [
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 Text(
                   'PRESETS',
                   textAlign: TextAlign.center,
                   style: context.theme.textTheme.headline5,
                 ),
-                GridView.count(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 10,
-                  controller: _scrollController,
-                  shrinkWrap: true,
-                  mainAxisSpacing: 10,
-                  padding: const EdgeInsets.all(10),
-                  children: [
-                    for (final category in state.data
-                            ?.where((element) =>
-                                element.type == VoicesCategoryType.preset)
-                            .toList() ??
-                        [])
-                      GridItem(
-                        title: category.title,
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (_) => VoiceSelectionPage(
-                                category: category,
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                  ],
+                _CategoryGrid(
+                  scrollController: _scrollController,
+                  categories: state.data
+                          ?.where((element) =>
+                              element.type == VoicesCategoryType.preset)
+                          .toList() ??
+                      [],
                 ),
-                // Text(
-                //   'CUSTOM',
-                //   textAlign: TextAlign.center,
-                //   style: context.theme.textTheme.headline5,
-                // ),
+                Text(
+                  'CUSTOM',
+                  textAlign: TextAlign.center,
+                  style: context.theme.textTheme.headline5,
+                ),
+                _CategoryGrid(
+                  scrollController: _scrollController,
+                  categories: state.data
+                          ?.where((element) =>
+                              element.type == VoicesCategoryType.custom)
+                          .toList() ??
+                      [],
+                ),
               ],
             );
           },
         ),
       ),
+    );
+  }
+}
+
+class _CategoryGrid extends StatelessWidget {
+  final ScrollController scrollController;
+  final List<VoicesCategory> categories;
+
+  const _CategoryGrid({
+    required this.scrollController,
+    required this.categories,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      crossAxisSpacing: 10,
+      controller: scrollController,
+      shrinkWrap: true,
+      mainAxisSpacing: 10,
+      padding: const EdgeInsets.all(10),
+      children: [
+        for (final category in categories)
+          GridItem(
+            title: category.title,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => VoiceSelectionPage(
+                    category: category,
+                  ),
+                ),
+              );
+            },
+          ),
+      ],
     );
   }
 }
