6edddb5f7162ac7c0cdc1fe3a49547489ffbdb9d
Jan Kollert <jan.kollert@digital.cz>
2022-10-04 14:52:04 +0200

    WIP: local directory handling

--- lib/data/repositories/custom_voices_repository.dart
+++ lib/data/repositories/custom_voices_repository.dart
@@ -1,12 +1,25 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:voice_pad/data/models/voice_line.dart';
 import 'package:voice_pad/data/models/voices_category.dart';
 import 'package:voice_pad/data/repositories/voices_repository.dart';
 
 class CustomVoicesRepository implements VoicesRepository {
   @override
-  Future<List<VoicesCategory>> getCategories() {
-    // TODO: implement getCategories
-    throw UnimplementedError();
+  Future<List<VoicesCategory>> getCategories() async {
+    final Directory appDocumentsDirectory =
+        await getApplicationDocumentsDirectory();
+    final Directory voicesCategoriesDirectory =
+        Directory('${appDocumentsDirectory.path}/voices');
+
+    // Creates directory if it does not exist
+    voicesCategoriesDirectory.createSync();
+
+    // Gets subdirectories
+    final categories = voicesCategoriesDirectory.listSync();
+
+    return filePath;
   }
 
   @override
